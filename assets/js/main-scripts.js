0030505bb2b7853a4c5dc431b531bcb7174f5fc0
Thomas de Mareuil <tdemareuil@hotmail.fr>
2020-04-05 18:56:22 +0200

    Update typed

--- assets/js/main-scripts.js
+++ assets/js/main-scripts.js
@@ -298,7 +298,7 @@
     MIRO.Typed1 = function() {
         if ($("#type1")[0]) {
             var element = new Typed("#type1", {
-                strings: ["Data Scientist", "X-HEC Student", "Curious & Creative mind!"],
+                strings: ["a Data Scientist", "an X-HEC Student", "Curious & Creative!"],
                 typeSpeed: 100,
                 backSpeed: 20,
                 loop: true
